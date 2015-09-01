4a800aba489d55889c2639019ea6932fe2678644
zhangzq <zhangzq@Ctrip.com>
2015-09-01 18:00:46 +0800

    razor compress

---
+++ RazorViewCompress/CompressHelper.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RazorViewCompress
+{
+    internal static class CompressHelper
+    {
+        private static string[] viewLocationFormats = new string[] {
+            "~/Views/{1}/{0}.cshtml",
+            "~/Views/{1}/{0}.vbhtml",
+            "~/Views/Shared/{0}.cshtml",
+             "~/Views/Shared/{0}.vbhtml" 
+        };
+        private static string[] areaViewLocationFormats = new string[] {
+             "~/Areas/{2}/Views/{1}/{0}.cshtml",
+            "~/Areas/{2}/Views/{1}/{0}.vbhtml",
+             "~/Areas/{2}/Views/Shared/{0}.cshtml",
+             "~/Areas/{2}/Views/Shared/{0}.vbhtml" 
+        };
+
+        public static string GetCompressedViewName(ControllerContext controllerContext, string viewName)
+        {
+            List<string> viewLocations = GetViewLocations(controllerContext, viewName);
+
+            foreach (string viewLocation in viewLocations)
+            {
+                string filePath = controllerContext.HttpContext.Request.MapPath(viewLocation);
+                if (File.Exists(filePath))
+                {
+                    var compressedViewLocation = viewLocation.Insert(viewLocation.LastIndexOf('/') + 1, "Temp");
+                    var compressedFilePath = HttpContext.Current.Request.MapPath(compressedViewLocation);
+
+                    TryCompress(filePath, compressedFilePath);
+                    return "Temp" + viewName;
+                }
+            }
+
+            //Find no exist file match the viewName in viewLocations 
+            return viewName;
+        }
+
+        private static List<string> GetViewLocations(ControllerContext controllerContext, string viewName)
+        {
+            List<string> viewLocations = new List<string>();
+
+            string controllerName = controllerContext.RouteData.GetRequiredString("controller");
+            Array.ForEach(viewLocationFormats, viewLocationFormat => viewLocations.Add(string.Format(viewLocationFormat, viewName, controllerName)));
+
+            object areaName;
+            if (controllerContext.RouteData.Values.TryGetValue("area", out areaName))
+            {
+                Array.ForEach(areaViewLocationFormats, areaViewLocationFormat => viewLocations.Add(string.Format(areaViewLocationFormat, viewName, controllerName, areaName)));
+            }
+
+            return viewLocations;
+        }
+
+        private static void TryCompress(string filePath, string compressedFilePath)
+        {
+
+            if (NeedCompress(filePath, compressedFilePath))
+            {
+                //CreateDirectory(compressedFilePath);
+                Compress(filePath, compressedFilePath);
+            }
+        }
+
+        private static bool NeedCompress(string filePath, string outputPath)
+        {
+            if (!File.Exists(outputPath))
+            {
+                return true;
+            }
+            return File.GetLastWriteTimeUtc(filePath) > File.GetLastWriteTimeUtc(outputPath);
+        }
+
+        private static void Compress(string filePath, string compressedFilePath)
+        {
+            var fileContent = File.ReadAllText(filePath);
+
+            fileContent = Regex.Replace(fileContent, @">\s+<", "><");
+            fileContent = Regex.Replace(fileContent, @"\r\n\s{0,}", " ");
+
+            using (var fileStream = File.Create(compressedFilePath))
+            {
+                using (var txtWriter = new StreamWriter(fileStream, Encoding.UTF8))
+                {
+                    txtWriter.WriteLine(fileContent);
+                }
+            }
+        }
+
+        private static void CreateDirectory(string filePath)
+        {
+            var directory = Path.GetDirectoryName(filePath);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+        }
+    }
+}
---
+++ RazorViewCompress/ViewEngineAgent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RazorViewCompress
+{
+    public class ViewEngineAgent : IViewEngine
+    {
+        private readonly IViewEngine _innerViewEngine;
+        public ViewEngineAgent()
+        {
+            _innerViewEngine = new RazorViewEngine();
+        }
+        public ViewEngineResult FindPartialView(ControllerContext controllerContext, string partialViewName, bool useCache)
+        {
+            partialViewName = CompressHelper.GetCompressedViewName(controllerContext, partialViewName);
+
+            var result = _innerViewEngine.FindPartialView(controllerContext, partialViewName, useCache);
+            return result;
+        }
+        public ViewEngineResult FindView(ControllerContext controllerContext, string viewName, string masterName, bool useCache)
+        {
+            if (masterName != null)
+            {
+                masterName = masterName == string.Empty ? "_Layout" : masterName;
+            }
+            masterName = CompressHelper.GetCompressedViewName(controllerContext, masterName);
+            viewName = CompressHelper.GetCompressedViewName(controllerContext, viewName);
+
+            var result = _innerViewEngine.FindView(controllerContext, viewName, masterName, useCache);
+            return result;
+        }
+        public void ReleaseView(ControllerContext controllerContext, IView view)
+        {
+            _innerViewEngine.ReleaseView(controllerContext, view);
+        }
+    }
+}
